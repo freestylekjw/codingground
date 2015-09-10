011fa5199639993bbb16bfd5aff2425fbeac4b9c
freestylekjw <452487989@qq.com>
2015-09-10 17:28:58 +0800

    Created by Coding Ground Client

---
+++ LinkedList/MyLinkedList.java
@@ -0,0 +1,28 @@
+public class MyLinkedList{
+
+    private class Data
+    {
+        private Object data;
+        private Data next=null;
+        Data(Object obj)
+        {
+            this.data=obj;
+        }
+        Data(Object obj,Data data)
+        {
+            this.data=obj;
+            this.data=data;
+        }
+    }
+    
+    private Data firstData=null;
+    
+    public void add(Object obj)
+    {
+        if(firstData==null)
+        {
+            firstData=new Data(obj);
+        }
+    }
+    
+}
