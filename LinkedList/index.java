c633d0a7c127961444d0e3d2e5643492377738fe
freestylekjw <452487989@qq.com>
2015-09-11 09:00:08 +0800

    Created by Coding Ground Client

---
+++ LinkedList/index.java
@@ -0,0 +1,9 @@
+public class index
+{
+    public static void main(String a[])
+    {
+        MyLinkedList list=new MyLinkedList();
+        list.add(22);
+        System.out.println(list);
+    }
+}
